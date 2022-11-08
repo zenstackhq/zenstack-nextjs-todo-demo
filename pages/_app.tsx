a1b260d396b493fae7db5be92e787df3c79c5a54
js <jiashengguo@outlook.com>
2022-11-08 16:32:44 +0000

    add auth guard

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,16 +4,25 @@ import NavBar from "components/NavBar";
 import { useCurrentUser, UserContext } from "@lib/context";
 import type { AppProps } from "next/app";
 import type { Session } from "next-auth";
+import AuthGuard from "../components/AuthGuard";
+import { useRouter } from "next/router";
 
 function AppContent(props: { children: JSX.Element | JSX.Element[] }) {
+  const router = useRouter();
   const user = useCurrentUser();
+  if (router.pathname == "/login") {
+    return <UserContext.Provider value={user}>{props.children}</UserContext.Provider>;
+  }
+
   return (
-    <UserContext.Provider value={user}>
-      <div className="h-screen flex flex-col">
-        <NavBar user={user} />
-        {props.children}
-      </div>
-    </UserContext.Provider>
+    <AuthGuard>
+      <UserContext.Provider value={user}>
+        <div className="h-screen flex flex-col">
+          <NavBar user={user} />
+          {props.children}
+        </div>
+      </UserContext.Provider>
+    </AuthGuard>
   );
 }
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,16 @@
 import React from "react";
-import { signIn, useSession } from "next-auth/react";
+import { useCurrentUser } from "../lib/context";
 
 export default function Home() {
-  const { status, data } = useSession();
+  const user = useCurrentUser();
 
-  if (status === "loading") {
-    return <p>Loading...</p>;
-  } else if (status === "unauthenticated") {
-    signIn();
-    return <></>;
-  } else {
-    const user = data?.user;
-    return (
-      <>
-        {user && (
-          <div className="mt-8 text-center flex flex-col items-center w-full">
-            <h1 className="text-2xl text-gray-800">Welcome {user.name || user.email}!</h1>
-          </div>
-        )}
-      </>
-    );
-  }
+  return (
+    <>
+      {user && (
+        <div className="mt-8 text-center flex flex-col items-center w-full">
+          <h1 className="text-2xl text-gray-800">Welcome {user.name || user.email}!</h1>
+        </div>
+      )}
+    </>
+  );
 }
