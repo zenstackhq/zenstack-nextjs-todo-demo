135d00843da193220694e3328002df2c6a9e5d55
JG <jiashengguo@outlook.com>
2022-11-09 10:53:04 +0000

    Fix policy & generate constant user avatar

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -33,7 +33,7 @@ export default function TodoList({ value }: Props) {
     <div className="card w-80 bg-base-100 shadow-xl cursor-pointer hover:bg-gray-50">
       <a>
         <figure>
-          <Image src={`https://picsum.photos/300/200?r=${value.title}`} width={320} height={200} alt="Cover" />
+          <Image src={`https://picsum.photos/300/200?r=${value.id}`} width={320} height={200} alt="Cover" />
         </figure>
       </a>
       <div className="card-body">
